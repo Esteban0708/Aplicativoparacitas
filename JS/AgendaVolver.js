28220ae1759f560764f8f38d39b7fea2f0c73d9f
AngieVelasquez <an.velasquez@udla.edu.co>
2023-11-08 11:07:33 -0500

    Cambios en citas y agenda

---
+++ JS/AgendaVolver.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const volverButton = document.getElementById('Volver');
+    
+    volverButton.addEventListener('click', function() {
+        window.location.href = 'Agenda.html';
+    });
+});
